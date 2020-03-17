419f2d85cc455b8738405eceaa9e6dfe2f655410
unknown <hesham14yahia@gmail.com>
2020-03-17 23:38:34 +0200

    add_article_control

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -12,10 +12,70 @@
                 </small>
             </div>
 
+            @if (\Illuminate\Support\Str::contains(request()->url(), 'profile'))
+            <!-- Article control -->
+            <div class="pull-right">
+                <!-- Single button -->
+                <div class="btn-group post-control">
+                    <button type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-cog" aria-hidden="true"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a data-toggle="modal" href="#modal-id">
+                                <i class="fa fa-edit fa-lg" aria-hidden="true"></i> Edit Post
+                            </a>
+                        </li>
+                        <li role="separator" class="divider"></li>
+                        <li>
+                            <a href="javascript:;">
+                                <i class="fa fa-trash fa-lg" aria-hidden="true"></i> Delete Post
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Edit Post Form -->
+            <div class="modal fade" id="modal-id">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                                &times;
+                            </button>
+                            <h4 class="modal-title">Edit Post</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form action="" id="edit-post-id">
+                                <textarea name="" rows="5" class="form-control">
+                                    Post content
+                                </textarea>
+                                <br>
+                                <input type="file" class="hidden input-image" id="input-image-edit">
+                                <label for="input-image" class="upload-label" title="upload photo">
+                                    <img data-src="holder.js/100x100?text=Upload Photo" class="img img-rounded img-thumbnail">
+                                </label>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" form="edit-post-id" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+            <!-- Edit Post Form -->
+
+            <!-- /Article control -->
+            @endif
+
         </div>
 
     </div>
-    <!-- Post content-->
+
+    <!-- Article content-->
     <div class="panel-body">
         @if ($article->image !== null)
             <img src="{{ $article->imageUrl }}" class="img img-responsive">
@@ -25,9 +85,10 @@
             {{ $article->body }}
         </p>
     </div>
-    <!-- /Post content-->
+    <!-- /Article content-->
 
     <div class="panel-footer">
+
         <!-- Like Button -->
         <span class="like-btn">
             <small class="text-muted likes">{{ $article->likes_count() }}</small>
@@ -44,7 +105,6 @@
                 <i class="fa fa-thumbs-o-down fa-lg" aria-hidden="true"></i>
             </a>
         </span>
-        <hr>
         <!-- Dislike comment form -->
 
     </div>
